c97267395d89583fb9e4e0ed19b60863f8df746d
XYenon <20698483+xyenon@users.noreply.github.com>
2022-05-11 19:28:47 +0800

    Fix ss-server scripts

--- cli/ss-server/uninstall.sh
+++ cli/ss-server/uninstall.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl stop ss
 sudo systemctl stop 'ss@*'
-sudo rm /usr/local/bin/ss-server
-sudo rm -r /usr/local/etc/shadowsocks
-sudo rm /etc/systemd/system/ss.service
+sudo rm -rf /usr/local/bin/ss-server
+sudo rm -rf /usr/local/etc/shadowsocks
+sudo rm -rf /etc/systemd/system/ss.service
 sudo systemctl daemon-reload
