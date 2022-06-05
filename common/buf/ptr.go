e0f722558141a6bba46553021b464b63e899a29f
世界 <i@sekai.icu>
2022-06-05 09:08:00 +0800

    Fix disable invalidptr

--- common/buf/ptr.go
+++ common/buf/ptr.go
@@ -3,23 +3,28 @@
 package buf
 
 import (
-	"os"
+	"unsafe"
 	_ "unsafe"
 )
 
-//go:linkname parsedebugvars runtime.parsedebugvars
-func parsedebugvars()
-
-func init() {
-	disableInvalidPtrCheck()
+type dbgVar struct {
+	name  string
+	value *int32
 }
 
-func disableInvalidPtrCheck() {
-	debug := os.Getenv("GODEBUG")
-	if debug == "" {
-		os.Setenv("GODEBUG", "invalidptr=0")
-	} else {
-		os.Setenv("GODEBUG", debug+",invalidptr=0")
+//go:linkname dbgvars runtime.dbgvars
+var dbgvars any
+
+// go.info.runtime.dbgvars: relocation target go.info.[]github.com/sagernet/sing/common/buf.dbgVar not defined
+//var dbgvars []dbgVar
+
+func init() {
+	debugVars := *(*[]dbgVar)(unsafe.Pointer(&dbgvars))
+	for _, v := range debugVars {
+		if v.name == "invalidptr" {
+			*v.value = 0
+			return
+		}
 	}
-	parsedebugvars()
+	panic("can't disable invalidptr")
 }
