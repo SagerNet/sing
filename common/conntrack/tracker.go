ce854cda8522159a6770b72e496fafd239b9681d
世界 <i@sekai.icu>
2022-08-17 20:46:08 +0800

    Add closer wrapper

--- common/conntrack/tracker.go
+++ common/conntrack/tracker.go
@@ -2,9 +2,12 @@ package conntrack
 
 import (
 	"io"
+	"net"
 	"sync"
 
 	"github.com/sagernet/sing/common"
+	"github.com/sagernet/sing/common/bufio"
+	N "github.com/sagernet/sing/common/network"
 	"github.com/sagernet/sing/common/x/list"
 )
 
@@ -18,6 +21,16 @@ func (m *Tracker) Track(conn io.Closer) *Registration {
 	element := m.connections.PushBack(conn)
 	m.access.Unlock()
 	return &Registration{m, element}
+}
+
+func (m *Tracker) TrackConn(conn net.Conn) net.Conn {
+	registration := m.Track(conn)
+	return &trackConn{conn, registration}
+}
+
+func (m *Tracker) TrackPacketConn(conn net.PacketConn) N.NetPacketConn {
+	registration := m.Track(conn)
+	return &trackPacketConn{bufio.NewPacketConn(conn), registration}
 }
 
 func (m *Tracker) Reset() {
@@ -38,4 +51,48 @@ func (t *Registration) Leave() {
 	t.manager.access.Lock()
 	defer t.manager.access.Unlock()
 	t.manager.connections.Remove(t.element)
+}
+
+type trackConn struct {
+	net.Conn
+	registration *Registration
+}
+
+func (t *trackConn) Close() error {
+	t.registration.Leave()
+	return t.Conn.Close()
+}
+
+func (t *trackConn) WriteTo(w io.Writer) (n int64, err error) {
+	return bufio.Copy(w, t.Conn)
+}
+
+func (t *trackConn) ReadFrom(r io.Reader) (n int64, err error) {
+	return bufio.Copy(t.Conn, r)
+}
+
+func (t *trackConn) Upstream() any {
+	return t.Conn
+}
+
+func (t *trackConn) ReaderReplaceable() bool {
+	return true
+}
+
+func (t *trackConn) WriterReplaceable() bool {
+	return true
+}
+
+type trackPacketConn struct {
+	N.NetPacketConn
+	registration *Registration
+}
+
+func (t *trackPacketConn) Close() error {
+	t.registration.Leave()
+	return t.NetPacketConn.Close()
+}
+
+func (t *trackPacketConn) Upstream() any {
+	return t.NetPacketConn
 }
