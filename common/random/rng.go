af83426404076849382c46efb99f7bb8d7e6d3f2
世界 <i@sekai.icu>
2023-02-07 11:45:27 +0800

    Remove seed initialize on go1.20

--- common/random/rng.go
+++ common/random/rng.go
@@ -1,10 +1,8 @@
 package random
 
 import (
-	"crypto/rand"
 	"encoding/binary"
 	"io"
-	mRand "math/rand"
 	"sync"
 
 	"github.com/sagernet/sing/common"
@@ -14,18 +12,6 @@ const (
 	rngMax  = 1 << 63
 	rngMask = rngMax - 1
 )
-
-var initSeedOnce sync.Once
-
-func InitializeSeed() {
-	initSeedOnce.Do(initializeSeed)
-}
-
-func initializeSeed() {
-	var seed int64
-	common.Must(binary.Read(rand.Reader, binary.LittleEndian, &seed))
-	mRand.Seed(seed)
-}
 
 type Source struct {
 	io.Reader
