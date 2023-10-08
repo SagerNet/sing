59101a440080248c926faa40129015d7d50516d9
世界 <i@sekai.icu>
2023-10-08 12:07:25 +0800

    Add http.parseBasicAuth func stub

--- protocol/http/link.go
+++ protocol/http/link.go
@@ -12,3 +12,6 @@ func ReadRequest(b *bufio.Reader) (req *http.Request, err error)
 
 //go:linkname URLSetPath net/url.(*URL).setPath
 func URLSetPath(u *url.URL, p string) error
+
+//go:linkname ParseBasicAuth net/http.parseBasicAuth
+func ParseBasicAuth(auth string) (username, password string, ok bool)
