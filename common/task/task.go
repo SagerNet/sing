96a05f9afefe9f990452eb33f24b9e44f4970dfe
世界 <i@sekai.icu>
2023-10-10 15:06:28 +0800

    Fix task cancel context

--- common/task/task.go
+++ common/task/task.go
@@ -88,6 +88,10 @@ func (g *Group) RunContextList(contextList []context.Context) error {
 
 	selectedContext, upstreamErr := common.SelectContext(append([]context.Context{taskCancelContext}, contextList...))
 
+	if selectedContext == 0 {
+		taskCancel(upstreamErr)
+	}
+
 	if g.cleanup != nil {
 		g.cleanup()
 	}
