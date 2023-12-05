9dc20a354ca144b04ed9d3374986c06dae068a57
世界 <i@sekai.icu>
2023-12-05 14:26:35 +0800

    Migrate json wrapper and badjson to library

---
+++ common/json/context.go
@@ -0,0 +1,23 @@
+//go:build go1.21 && !without_contextjson
+
+package json
+
+import (
+	"github.com/sagernet/sing/common/json/internal/contextjson"
+)
+
+var (
+	Marshal    = json.Marshal
+	Unmarshal  = json.Unmarshal
+	NewEncoder = json.NewEncoder
+	NewDecoder = json.NewDecoder
+)
+
+type (
+	Encoder     = json.Encoder
+	Decoder     = json.Decoder
+	Token       = json.Token
+	Delim       = json.Delim
+	SyntaxError = json.SyntaxError
+	RawMessage  = json.RawMessage
+)
