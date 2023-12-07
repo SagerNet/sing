14717bad08bb1594eb5c09f1af1939cb96d2c8f6
世界 <i@sekai.icu>
2023-12-07 18:01:10 +0800

    Enable read wait copy for windows

--- common/bufio/copy_direct.go
+++ common/bufio/copy_direct.go
@@ -1,12 +1,16 @@
 package bufio
 
 import (
+	"errors"
+	"io"
 	"syscall"
 
+	"github.com/sagernet/sing/common/buf"
+	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
 )
 
-func CopyDirect(source syscall.Conn, destination syscall.Conn, readCounters []N.CountFunc, writeCounters []N.CountFunc) (handed bool, n int64, err error) {
+func copyDirect(source syscall.Conn, destination syscall.Conn, readCounters []N.CountFunc, writeCounters []N.CountFunc) (handed bool, n int64, err error) {
 	rawSource, err := source.SyscallConn()
 	if err != nil {
 		return
@@ -17,4 +21,70 @@ func CopyDirect(source syscall.Conn, destination syscall.Conn, readCounters []N.
 	}
 	handed, n, err = splice(rawSource, rawDestination, readCounters, writeCounters)
 	return
+}
+
+func copyWaitWithPool(originSource io.Reader, destination N.ExtendedWriter, source N.ReadWaiter, readCounters []N.CountFunc, writeCounters []N.CountFunc) (handled bool, n int64, err error) {
+	handled = true
+	var (
+		buffer       *buf.Buffer
+		notFirstTime bool
+	)
+	for {
+		buffer, err = source.WaitReadBuffer()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				err = nil
+				return
+			}
+			return
+		}
+		dataLen := buffer.Len()
+		err = destination.WriteBuffer(buffer)
+		if err != nil {
+			buffer.Leak()
+			if !notFirstTime {
+				err = N.ReportHandshakeFailure(originSource, err)
+			}
+			return
+		}
+		n += int64(dataLen)
+		for _, counter := range readCounters {
+			counter(int64(dataLen))
+		}
+		for _, counter := range writeCounters {
+			counter(int64(dataLen))
+		}
+		notFirstTime = true
+	}
+}
+
+func copyPacketWaitWithPool(originSource N.PacketReader, destinationConn N.PacketWriter, source N.PacketReadWaiter, readCounters []N.CountFunc, writeCounters []N.CountFunc, notFirstTime bool) (handled bool, n int64, err error) {
+	handled = true
+	var (
+		buffer      *buf.Buffer
+		destination M.Socksaddr
+	)
+	for {
+		buffer, destination, err = source.WaitReadPacket()
+		if err != nil {
+			return
+		}
+		dataLen := buffer.Len()
+		err = destinationConn.WritePacket(buffer, destination)
+		if err != nil {
+			buffer.Leak()
+			if !notFirstTime {
+				err = N.ReportHandshakeFailure(originSource, err)
+			}
+			return
+		}
+		n += int64(dataLen)
+		for _, counter := range readCounters {
+			counter(int64(dataLen))
+		}
+		for _, counter := range writeCounters {
+			counter(int64(dataLen))
+		}
+		notFirstTime = true
+	}
 }
--- common/bufio/copy_direct_windows.go
+++ common/bufio/copy_direct_windows.go
@@ -1,18 +1,8 @@
 package bufio
 
 import (
-	"io"
-
 	N "github.com/sagernet/sing/common/network"
 )
-
-func copyWaitWithPool(originSource io.Reader, destination N.ExtendedWriter, source N.ReadWaiter, readCounters []N.CountFunc, writeCounters []N.CountFunc) (handled bool, n int64, err error) {
-	return
-}
-
-func copyPacketWaitWithPool(originSource N.PacketReader, destinationConn N.PacketWriter, source N.PacketReadWaiter, readCounters []N.CountFunc, writeCounters []N.CountFunc, notFirstTime bool) (handled bool, n int64, err error) {
-	return
-}
 
 func createSyscallReadWaiter(reader any) (N.ReadWaiter, bool) {
 	return nil, false
