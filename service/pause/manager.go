447bc3c965b45f40cdfae97cd7e3e1414bc73f9a
世界 <i@sekai.icu>
2023-12-15 01:09:36 +0800

    Improve pause manager

--- service/pause/manager.go
+++ service/pause/manager.go
@@ -1,12 +1,25 @@
 package pause
 
+import "github.com/sagernet/sing/common/x/list"
+
 type Manager interface {
 	DevicePause()
 	DeviceWake()
-	DevicePauseChan() <-chan struct{}
 	NetworkPause()
 	NetworkWake()
-	NetworkPauseChan() <-chan struct{}
+	IsDevicePaused() bool
+	IsNetworkPaused() bool
 	IsPaused() bool
 	WaitActive()
+	RegisterCallback(callback Callback) *list.Element[Callback]
+	UnregisterCallback(element *list.Element[Callback])
 }
+
+const (
+	EventDevicePaused int = iota
+	EventDeviceWake
+	EventNetworkPause
+	EventNetworkWake
+)
+
+type Callback = func(event int)
