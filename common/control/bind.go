27a25f7e8d5c9e117cc80c1cde8832558c26beed
世界 <i@sekai.icu>
2023-12-16 16:08:25 +0800

    Add 'preferInterfaceName' parameter to BindToInterface0

--- common/control/bind.go
+++ common/control/bind.go
@@ -10,7 +10,7 @@ import (
 
 func BindToInterface(finder InterfaceFinder, interfaceName string, interfaceIndex int) Func {
 	return func(network, address string, conn syscall.RawConn) error {
-		return BindToInterface0(finder, conn, network, address, interfaceName, interfaceIndex)
+		return BindToInterface0(finder, conn, network, address, interfaceName, interfaceIndex, false)
 	}
 }
 
@@ -20,16 +20,16 @@ func BindToInterfaceFunc(finder InterfaceFinder, block func(network string, addr
 		if err != nil {
 			return err
 		}
-		return BindToInterface0(finder, conn, network, address, interfaceName, interfaceIndex)
+		return BindToInterface0(finder, conn, network, address, interfaceName, interfaceIndex, false)
 	}
 }
 
-func BindToInterface0(finder InterfaceFinder, conn syscall.RawConn, network string, address string, interfaceName string, interfaceIndex int) error {
+func BindToInterface0(finder InterfaceFinder, conn syscall.RawConn, network string, address string, interfaceName string, interfaceIndex int, preferInterfaceName bool) error {
 	if interfaceName == "" && interfaceIndex == -1 {
 		return E.New("interface not found: ", interfaceName)
 	}
 	if addr := M.ParseSocksaddr(address).Addr; addr.IsValid() && N.IsVirtual(addr) {
 		return nil
 	}
-	return bindToInterface(conn, network, address, finder, interfaceName, interfaceIndex)
+	return bindToInterface(conn, network, address, finder, interfaceName, interfaceIndex, preferInterfaceName)
 }
