10f242e3c9c44da6ca06a10cdc7eab3d96e92008
世界 <i@sekai.icu>
2024-04-08 17:26:08 +0800

    Add control.SetKeepAlivePeriod

---
+++ common/control/tcp_keep_alive_linux.go
@@ -0,0 +1,29 @@
+package control
+
+import (
+	N "github.com/sagernet/sing/common/network"
+	"syscall"
+	"time"
+	_ "unsafe"
+
+	E "github.com/sagernet/sing/common/exceptions"
+
+	"golang.org/x/sys/unix"
+)
+
+func SetKeepAlivePeriod(idle time.Duration, interval time.Duration) Func {
+	return func(network, address string, conn syscall.RawConn) error {
+		if N.NetworkName(network) != N.NetworkTCP {
+			return nil
+		}
+		return Raw(conn, func(fd uintptr) error {
+			return E.Errors(
+				unix.SetsockoptInt(int(fd), unix.IPPROTO_TCP, unix.TCP_KEEPIDLE, int(roundDurationUp(idle, time.Second))),
+				unix.SetsockoptInt(int(fd), unix.IPPROTO_TCP, unix.TCP_KEEPINTVL, int(roundDurationUp(interval, time.Second))),
+			)
+		})
+	}
+}
+
+//go:linkname roundDurationUp net.roundDurationUp
+func roundDurationUp(d time.Duration, to time.Duration) time.Duration
