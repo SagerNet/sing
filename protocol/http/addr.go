e8e8355879522ecc99e0635e032ab754d854a492
Reza Feizi <RezaFeizi@iran.ir>
2024-06-03 03:32:08 +0330

    Get Real User IP Over CDN
    More Info:
    https://developers.cloudflare.com/fundamentals/reference/http-request-headers/#x-forwarded-for

--- protocol/http/addr.go
+++ protocol/http/addr.go
@@ -11,11 +11,13 @@ func SourceAddress(request *http.Request) M.Socksaddr {
 	address := M.ParseSocksaddr(request.RemoteAddr)
 	forwardFrom := request.Header.Get("X-Forwarded-For")
 	if forwardFrom != "" {
-		for _, from := range strings.Split(forwardFrom, ",") {
-			originAddr := M.ParseAddr(from)
-			if originAddr.IsValid() {
-				address.Addr = originAddr
-			}
+		// Get the leftmost IP, which is the client's original IP address
+		ips := strings.Split(forwardFrom, ",")
+		// Trim any leading or trailing spaces from the IP address
+		originalClientIP := strings.TrimSpace(ips[0])
+		originAddr := M.ParseAddr(originalClientIP)
+		if originAddr.IsValid() {
+			address.Addr = originAddr
 		}
 	}
 	return address.Unwrap()
