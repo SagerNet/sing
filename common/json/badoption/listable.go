1ccfbdb861ea058b42a71c9574a636becea8b93d
世界 <i@sekai.icu>
2024-11-16 12:16:33 +0800

    badjson: Fix Listable

--- common/json/badoption/listable.go
+++ common/json/badoption/listable.go
@@ -9,24 +9,24 @@ import (
 
 type Listable[T any] []T
 
-func (l Listable[T]) MarshalJSON() ([]byte, error) {
+func (l Listable[T]) MarshalJSONContext(ctx context.Context) ([]byte, error) {
 	arrayList := []T(l)
 	if len(arrayList) == 1 {
 		return json.Marshal(arrayList[0])
 	}
-	return json.Marshal(arrayList)
+	return json.MarshalContext(ctx, arrayList)
 }
 
 func (l *Listable[T]) UnmarshalJSONContext(ctx context.Context, content []byte) error {
-	err := json.UnmarshalContextDisallowUnknownFields(ctx, content, (*[]T)(l))
+	var singleItem T
+	err := json.UnmarshalContextDisallowUnknownFields(ctx, content, &singleItem)
+	if err == nil {
+		*l = []T{singleItem}
+		return nil
+	}
+	newErr := json.UnmarshalContextDisallowUnknownFields(ctx, content, (*[]T)(l))
 	if err == nil {
 		return nil
 	}
-	var singleItem T
-	newError := json.UnmarshalContextDisallowUnknownFields(ctx, content, &singleItem)
-	if newError != nil {
-		return E.Errors(err, newError)
-	}
-	*l = []T{singleItem}
-	return nil
+	return E.Errors(err, newErr)
 }
