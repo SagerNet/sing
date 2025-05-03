3b2f04551ddc35a9b27c5574205fb0e5e640a87d
世界 <i@sekai.icu>
2025-05-03 18:53:34 +0800

    Add E.Cause1

--- common/exceptions/cause.go
+++ common/exceptions/cause.go
@@ -12,3 +12,16 @@ func (e *causeError) Error() string {
 func (e *causeError) Unwrap() error {
 	return e.cause
 }
+
+type causeError1 struct {
+	error
+	cause error
+}
+
+func (e *causeError1) Error() string {
+	return e.error.Error() + ": " + e.cause.Error()
+}
+
+func (e *causeError1) Unwrap() []error {
+	return []error{e.error, e.cause}
+}
