716ee8a0d394967ebc81f8c37c25699522a8677e
世界 <i@sekai.icu>
2025-06-15 16:59:12 +0800

    Add badjson.UnmarshallExcludedMulti

--- common/json/badjson/merge_objects.go
+++ common/json/badjson/merge_objects.go
@@ -54,6 +54,41 @@ func UnmarshallExcludedContext(ctx context.Context, inputContent []byte, parentO
 	return json.UnmarshalContextDisallowUnknownFields(ctx, inputContent, object)
 }
 
+func UnmarshallExcludedMulti(inputContent []byte, parentObject any, object any) error {
+	return UnmarshallExcludedContextMulti(context.Background(), inputContent, parentObject, object)
+}
+
+func UnmarshallExcludedContextMulti(ctx context.Context, inputContent []byte, parentObject any, object any) error {
+	var content JSONObject
+	err := content.UnmarshalJSONContext(ctx, inputContent)
+	if err != nil {
+		return err
+	}
+	parentBinary, err := json.MarshalContext(ctx, parentObject)
+	if err != nil {
+		return err
+	}
+	var parentMap map[string]any
+	err = json.UnmarshalContext(ctx, parentBinary, &parentMap)
+	if err != nil {
+		return err
+	}
+	for key := range parentMap {
+		content.Remove(key)
+	}
+	if object == nil {
+		if content.IsEmpty() {
+			return nil
+		}
+		return E.New("unexpected key: ", content.Keys()[0])
+	}
+	inputContent, err = content.MarshalJSONContext(ctx)
+	if err != nil {
+		return err
+	}
+	return json.UnmarshalContextDisallowUnknownFields(ctx, inputContent, object)
+}
+
 func newJSONObject(ctx context.Context, object any) (*JSONObject, error) {
 	inputContent, err := json.MarshalContext(ctx, object)
 	if err != nil {
