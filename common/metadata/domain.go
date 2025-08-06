670194bc930728c080bf7871b45bbdb19f95ae39
garmr <pdixon117@gmail.com>
2025-08-06 12:17:13 -0700

    export IsDomain

--- common/metadata/domain.go
+++ common/metadata/domain.go
@@ -1,17 +1,17 @@
 package metadata
 
-// isDomainName checks if a string is a presentation-format domain name
+// IsDomainName checks if a string is a presentation-format domain name
 // (currently restricted to hostname-compatible "preferred name" LDH labels and
 // SRV-like "underscore labels"; see golang.org/issue/12421).
 //
-// isDomainName should be an internal detail,
+// IsDomainName should be an internal detail,
 // but widely used packages access it using linkname.
 // Notable members of the hall of shame include:
 //   - github.com/sagernet/sing
 //
 // Do not remove or change the type signature.
 // See go.dev/issue/67401.
-func isDomainName(s string) bool {
+func IsDomainName(s string) bool {
 	// The root domain name is valid. See golang.org/issue/45715.
 	if s == "." {
 		return true
