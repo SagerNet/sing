d967575cebfa1b34cdf7e62598ab2a7a89afdd77
世界 <i@sekai.icu>
2025-08-14 09:42:49 +0800

    Remove compatibility code for Go versions prior to 1.20

--- common/atomic/typed.go
+++ common/atomic/typed.go
@@ -6,45 +6,41 @@ import (
 	"github.com/sagernet/sing/common"
 )
 
-type TypedValue[T any] struct {
-	value atomic.Value
-}
-
-// typedValue is a struct with determined type to resolve atomic.Value usages with interface types
-// https://github.com/golang/go/issues/22550
-//
-// The intention to have an atomic value store for errors. However, running this code panics:
-// panic: sync/atomic: store of inconsistently typed value into Value
-// This is because atomic.Value requires that the underlying concrete type be the same (which is a reasonable expectation for its implementation).
-// When going through the atomic.Value.Store method call, the fact that both these are of the error interface is lost.
-type typedValue[T any] struct {
-	value T
-}
+type TypedValue[T any] atomic.Pointer[T]
 
 func (t *TypedValue[T]) Load() T {
-	value := t.value.Load()
+	value := (*atomic.Pointer[T])(t).Load()
 	if value == nil {
 		return common.DefaultValue[T]()
 	}
-	return value.(typedValue[T]).value
+	return *value
 }
 
 func (t *TypedValue[T]) Store(value T) {
-	t.value.Store(typedValue[T]{value})
+	(*atomic.Pointer[T])(t).Store(&value)
 }
 
 func (t *TypedValue[T]) Swap(new T) T {
-	old := t.value.Swap(typedValue[T]{new})
+	old := (*atomic.Pointer[T])(t).Swap(&new)
 	if old == nil {
 		return common.DefaultValue[T]()
 	}
-	return old.(typedValue[T]).value
+	return *old
 }
 
 func (t *TypedValue[T]) CompareAndSwap(old, new T) bool {
-	return t.value.CompareAndSwap(typedValue[T]{old}, typedValue[T]{new}) ||
-		// In the edge-case where [atomic.Value.Store] is uninitialized
-		// and trying to compare with the zero value of T,
-		// then compare-and-swap with the nil any value.
-		(any(old) == any(common.DefaultValue[T]()) && t.value.CompareAndSwap(any(nil), typedValue[T]{new}))
+	for {
+		currentP := (*atomic.Pointer[T])(t).Load()
+		currentValue := common.DefaultValue[T]()
+		if currentP != nil {
+			currentValue = *currentP
+		}
+		// Compare old and current via runtime equality check.
+		if any(currentValue) != any(old) {
+			return false
+		}
+		if (*atomic.Pointer[T])(t).CompareAndSwap(currentP, &new) {
+			return true
+		}
+	}
 }
--- common/atomic/types.go
+++ common/atomic/types.go
@@ -1,19 +1,20 @@
-//go:build go1.19
+//go:build go1.24
 
 package atomic
 
 import "sync/atomic"
 
 type (
-	Bool    = atomic.Bool
-	Int32   = atomic.Int32
-	Int64   = atomic.Int64
-	Uint32  = atomic.Uint32
-	Uint64  = atomic.Uint64
-	Uintptr = atomic.Uintptr
-	Value   = atomic.Value
+	Bool           = atomic.Bool
+	Int32          = atomic.Int32
+	Int64          = atomic.Int64
+	Uint32         = atomic.Uint32
+	Uint64         = atomic.Uint64
+	Uintptr        = atomic.Uintptr
+	Value          = atomic.Value
+	Pointer[T any] = atomic.Pointer[T]
 )
 
-type Pointer[T any] struct {
-	atomic.Pointer[T]
+func PointerForm[T any](p *atomic.Pointer[T]) *Pointer[T] {
+	return p
 }
