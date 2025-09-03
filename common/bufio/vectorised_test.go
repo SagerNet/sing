781be73c059fa6a7593bcc8ed18ff09c9a053345
世界 <i@sekai.icu>
2025-09-03 09:49:28 +0800

    Fix SyscallVectorisedPacketWriter

--- common/bufio/vectorised_test.go
+++ common/bufio/vectorised_test.go
@@ -61,3 +61,29 @@ func TestWriteVectorisedPacket(t *testing.T) {
 	require.Equal(t, 2048, n)
 	require.Equal(t, bufC[:], output)
 }
+
+func TestWriteVectorisedPacket6(t *testing.T) {
+	t.Parallel()
+	inputConn, outputConn, outputAddr := UDPPipe6(t)
+	vectorisedWriter, created := CreateVectorisedPacketWriter(inputConn)
+	require.True(t, created)
+	require.NotNil(t, vectorisedWriter)
+	var bufA [1024]byte
+	var bufB [1024]byte
+	var bufC [2048]byte
+	_, err := io.ReadFull(rand.Reader, bufA[:])
+	require.NoError(t, err)
+	_, err = io.ReadFull(rand.Reader, bufB[:])
+	require.NoError(t, err)
+	copy(bufC[:], bufA[:])
+	copy(bufC[1024:], bufB[:])
+	finish := Timeout(t)
+	_, err = WriteVectorisedPacket(vectorisedWriter, [][]byte{bufA[:], bufB[:]}, outputAddr)
+	require.NoError(t, err)
+	output := make([]byte, 2048)
+	n, _, err := outputConn.ReadFrom(output)
+	finish()
+	require.NoError(t, err)
+	require.Equal(t, 2048, n)
+	require.Equal(t, bufC[:], output)
+}
