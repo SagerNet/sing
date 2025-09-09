4c74a54d2af73e22e92b61ec11da0328118f5896
世界 <i@sekai.icu>
2025-09-09 20:38:07 +0800

    Improve early conn interface

--- common/network/early.go
+++ common/network/early.go
@@ -1,5 +1,38 @@
 package network
 
+import (
+	"io"
+
+	"github.com/sagernet/sing/common"
+)
+
+// Deprecated: use EarlyReader and EarlyWriter instead.
 type EarlyConn interface {
 	NeedHandshake() bool
+}
+
+type EarlyReader interface {
+	NeedHandshakeForRead() bool
+}
+
+func NeedHandshakeForRead(reader io.Reader) bool {
+	if earlyReader, isEarlyReader := common.Cast[EarlyReader](reader); isEarlyReader && earlyReader.NeedHandshakeForRead() {
+		return true
+	}
+	return false
+}
+
+type EarlyWriter interface {
+	NeedHandshakeForWrite() bool
+}
+
+func NeedHandshakeForWrite(writer io.Writer) bool {
+	if //goland:noinspection GoDeprecation
+	earlyConn, isEarlyConn := writer.(EarlyConn); isEarlyConn {
+		return earlyConn.NeedHandshake()
+	}
+	if earlyWriter, isEarlyWriter := common.Cast[EarlyWriter](writer); isEarlyWriter && earlyWriter.NeedHandshakeForWrite() {
+		return true
+	}
+	return false
 }
