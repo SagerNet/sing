a5a1cfa1f3c235f6806a0a6ce2dccae91629958e
xchacha20-poly1305 <HystericalDragons@proton.me>
2026-01-19 15:30:08 +0800

    varbin: Fix array item size

--- common/varbin/data.go
+++ common/varbin/data.go
@@ -393,7 +393,7 @@ func intItemBaseDataSize(data reflect.Value) int {
 		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 		reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
-		return itemType.Len()
+		return int(itemType.Size())
 	default:
 		return -1
 	}
