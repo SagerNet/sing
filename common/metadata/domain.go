a5e06ac852b6dab778676e0f0d3896f6ba31f2c3
世界 <i@sekai.icu>
2026-03-01 19:41:08 +0800

    metadata: Relax domain name validation to support non-standard characters

--- common/metadata/domain.go
+++ common/metadata/domain.go
@@ -1,6 +1,27 @@
 package metadata
 
-import _ "unsafe" // for linkname
-
-//go:linkname IsDomainName net.isDomainName
-func IsDomainName(domain string) bool
+func IsDomainName(domain string) bool {
+	l := len(domain)
+	if l == 0 || l > 254 {
+		return false
+	}
+	labelLength := 0
+	for i := 0; i < l; i++ {
+		c := domain[i]
+		if c == '.' {
+			if labelLength == 0 {
+				return false
+			}
+			labelLength = 0
+			continue
+		}
+		if c == 0 {
+			return false
+		}
+		labelLength++
+		if labelLength > 63 {
+			return false
+		}
+	}
+	return labelLength > 0
+}
