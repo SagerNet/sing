e90ad33f0423620c9d3983948200ff6db3beae58
Huge AGI7 <huge@agi7.ai>
2026-06-23 18:11:40 +0800

    protocol/http: buffer 407 response and preserve auth retry/logging

--- protocol/http/handshake.go
+++ protocol/http/handshake.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	std_bufio "bufio"
+	"bytes"
 	"context"
 	"io"
 	"net"
@@ -92,9 +93,9 @@ func HandleConnectionExWithOptions(
 				}
 				proxyAuthRequiredResponse := responseWith(request, http.StatusProxyAuthRequired, headers...)
 				printResponseHeaders(ctx, options.Logger, proxyAuthRequiredResponse)
-				err = proxyAuthRequiredResponse.Write(conn)
+				err = writeResponseBuffered(conn, proxyAuthRequiredResponse)
 				if err != nil {
-					return err
+					return E.Cause(err, "write proxy authentication required response")
 				}
 				authorization := request.Header.Get("Proxy-Authorization")
 				switch {
@@ -372,6 +373,22 @@ func removeExtraHTTPHostPort(req *http.Request) {
 
 	req.Host = host
 	req.URL.Host = host
+}
+
+func writeResponseBuffered(conn net.Conn, response *http.Response) error {
+	var responseBuffer bytes.Buffer
+	err := response.Write(&responseBuffer)
+	if err != nil {
+		return err
+	}
+	n, err := conn.Write(responseBuffer.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != responseBuffer.Len() {
+		return io.ErrShortWrite
+	}
+	return nil
 }
 
 func responseWith(request *http.Request, statusCode int, headers ...string) *http.Response {
