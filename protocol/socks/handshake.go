d4666f9616b8e9a5ab158f4f132d7711bd28e66d
世界 <i@sekai.icu>
2026-09-06 15:51:31 +0800

    socks: Report the UDP peer as source of associate connections

--- protocol/socks/handshake.go
+++ protocol/socks/handshake.go
@@ -247,7 +247,8 @@ func HandleConnectionEx(
 			if err != nil {
 				return E.Cause(err, "socks5: write response")
 			}
-			associateConn := NewAssociatePacketConn(bufio.NewServerPacketConn(udpConn), M.Socksaddr{}, conn)
+			serverConn := bufio.NewServerPacketConn(udpConn)
+			associateConn := NewAssociatePacketConn(serverConn, M.Socksaddr{}, conn)
 			go func() {
 				var buffer [1]byte
 				_, _ = conn.Read(buffer[:])
@@ -271,7 +272,7 @@ func HandleConnectionEx(
 				ctx, socksPacketConn = canceler.NewPacketConn(ctx, socksPacketConn, udpTimeout)
 			}
 			socksPacketConn = bufio.NewCachedPacketConn(socksPacketConn, firstPacket, destination)
-			handler.NewPacketConnectionEx(ctx, socksPacketConn, source, destination, onClose)
+			handler.NewPacketConnectionEx(ctx, socksPacketConn, M.SocksaddrFromNet(serverConn.RemoteAddr()).Unwrap(), destination, onClose)
 			return nil
 		/*case CommandTorResolve, CommandTorResolvePTR:
 		if resolver == nil {
