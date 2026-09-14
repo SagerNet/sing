5e936585cbf7132494721f3b2233dd5d28e898e5
世界 <i@sekai.icu>
2026-09-14 21:52:46 +0800

    control: Ignore UDP socket buffer failures

--- common/control/socket_buffer_linux.go
+++ common/control/socket_buffer_linux.go
@@ -1,25 +1,16 @@
 package control
 
 import (
-	"os"
-
 	"golang.org/x/sys/unix"
 )
 
-func setSocketBuffer(fd uintptr, size int) error {
+func setSocketBuffer(fd uintptr, size int) {
 	err := unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_RCVBUFFORCE, size)
 	if err != nil {
-		err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_RCVBUF, size)
-	}
-	if err != nil {
-		return os.NewSyscallError("SETSOCKOPT SO_RCVBUF", err)
+		_ = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_RCVBUF, size)
 	}
 	err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_SNDBUFFORCE, size)
 	if err != nil {
-		err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_SNDBUF, size)
+		_ = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_SNDBUF, size)
 	}
-	if err != nil {
-		return os.NewSyscallError("SETSOCKOPT SO_SNDBUF", err)
-	}
-	return nil
 }
